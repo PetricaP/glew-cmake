43040bd7ea3cb4c0d0a9dae6307a1b2b4f9f7813
Petru Petrica <petricap.gm@gmail.com>
2018-12-30 00:08:24 +0200

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+project "GLEW"
+    kind "StaticLib"
+    language "C"
+    
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GL/glew.h",
+		"src/glew.c"
+    }
+
+	includedirs {
+		"include",
+	}
+
+	defines {
+		"GLEW_STATIC"
+	}
+    
+	filter "system:windows"
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "10.0.17134.0"
+        staticruntime "On"
+        
+        files
+        {
+        }
+
+		defines 
+		{ 
+            "_CRT_SECURE_NO_WARNINGS"
+		}
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
+	filter "system:linux"
+        buildoptions { "-std=gnu17", "-lrt", "-lm", "-fPIC" }
+        staticruntime "On"
+        
+        files
+        {
+			"include/glxew.h"
+        }
+
+		links
+		{
+			"X11",
+			"Xext",
+			"dl"
+		}
+
+		defines 
+		{ 
+			"GLEW_NO_GLU"
+		}
